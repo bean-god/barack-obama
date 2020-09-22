7287b36ba4b2788ad13042db7c73c1b996fe2e50
bean-god <beengbean7@gmail.com>
2020-09-22 13:51:13 -0700

    Update message.js

--- events/message.js
+++ events/message.js
@@ -19,7 +19,7 @@ function pwait(iMilliSeconds) {
 module.exports = (client, message) => {
 var y = Math.floor(Math.random() * 2);
   if (message.content.includes("yes or no")) {
-    if (y == 1) {message.reply(`yes`)}else {message.reply(`no`)}}
+    if (y == 1) {message.channel.send(`yes`)}else {message.reply(`no`)}}
 const guild = message.guild;
 const member = message.member;
 const author = message.author;
